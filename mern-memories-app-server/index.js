0526f0df06b4d47400c40e08dd8c6010a790a3ca
Mümin <zehir.m.54@gmail.com>
2021-11-22 23:23:03 +0300

    Backend'de atılan istlerdeki cors engeli düzeltildi...

--- mern-memories-app-server/index.js
+++ mern-memories-app-server/index.js
@@ -2,15 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import memoryRouter from "./routers/memoryRouter.js";
 
 dotenv.config();
-
 const app = express();
 
 app.use(express.json({ limit: "20mb" }));
-
+app.use(cors());
 app.use("/memories", memoryRouter);
 
 // app.get("/", (req, res) => {
